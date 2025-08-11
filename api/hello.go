390dd60fa83b5ef0f47849fbb7f8a1d752aff9fa
samdo817 <phantom72008@gmail.com>
2025-08-11 14:24:58 +0700

    error vercel

--- api/hello.go
+++ api/hello.go
@@ -1,11 +1,10 @@
 package handler
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, `{"message": "Hello from Go on Vercel!"}`)
+    fmt.Fprintf(w, "Hello from Go API on Vercel!")
 }
